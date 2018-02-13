c103af60dc60a73b85824d3235a590a8122a21c2
VERANIKA <veranika.isakova89@gmail.com>
2018-02-13 15:15:16 +0100

    Add clock on the page with changing colors

---
+++ clock.js
@@ -0,0 +1,22 @@
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    changeBackground(h, m, s);
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML = h+":"+m+":"+s;
+    setTimeout(function(){startTime()},500);
+}
+function changeBackground(h, m, s){
+	var totalMins = (h * 60) + m;
+	var hue = totalMins * 0.25;
+	var sat = m + 20;
+	var light = s + 15;
+	document.body.style.backgroundColor= "hsl(" + hue + ", " + sat + "%, " + light + "%)";
+}
+function checkTime(i) {
+    if (i<10) {i = "0" + i};
+    return i;
+}
